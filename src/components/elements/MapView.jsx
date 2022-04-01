d1d435a4edda56904e979adcdf62d982e6dc6046
Maestroharyor <haryor1josh@gmail.com>
2022-04-01 14:10:34 +0100

    Error fix

--- src/components/elements/MapView.jsx
+++ src/components/elements/MapView.jsx
@@ -1,22 +1,13 @@
 import React, { useState, useContext } from "react";
 import { ContactContext } from "../../context/ContactContext";
 // const Map = React.lazy(() => import("react-map-gl"));
-import Map from 'react-map-gl';
+// import Map from 'react-map-gl';
 
 function MapView() {
   const [contacts, setContacts] = useContext(ContactContext);
   return (
     <div>
-      <Map
-        initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5
-        }}
-        style={{ width: 600, height: 400 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
-      ;
+      Map view
     </div>
   );
 }
